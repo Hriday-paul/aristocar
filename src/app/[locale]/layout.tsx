a17b48779773f721b2ae12292c1687578a3d6e34
Hriday-paul <hridoychandrapaul.10@gmail.com>
2024-12-10 10:05:15 +0600

    fix build issue

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,42 +1,44 @@
-import Navbar from '@/components/shared/Navbar/Navbar';
-import React from 'react';
-import { getLocale, getTranslations } from "next-intl/server";
-import Footer from '@/components/shared/Footer/Footer';
+import localFont from "next/font/local";
+import ReduxStoreProvider from '@/provider/ReduxStoreProvider';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
 
-const layout = async ({ children }: { children: React.ReactNode }) => {
-    const locales = await getLocale();
-    const t = await getTranslations('navbar');
+const Lastica = localFont({
+  src: "../../../font/Lastica.ttf",
+  variable: "--font-lastica",
+  weight: "100 400 900",
+});
+const Poppins = localFont({
+  src: "../../../font/Poppins-Regular.ttf",
+  variable: "--font-poppins",
+  weight: "100 200 900",
+});
+const Satrosi = localFont({
+  src: "../../../font/Satoshi-Regular.ttf",
+  variable: "--font-satoshi",
+  weight: "100 200 300 400 500 600 700 800 900",
+});
 
-    const routs: { id: number, name: string, rout: string }[] = [
-        {
-            id: 1,
-            name: t('Home'),
-            rout: '/'
-        },
-        {
-            id: 3,
-            name: t('Sell'),
-            rout: '/sell'
-        },
-        {
-            id: 4,
-            name: t('About us'),
-            rout: '/about'
-        },
-        {
-            id: 5,
-            name: t('Support'),
-            rout: '/support'
-        },
+export default async function LocaleLayout({
+  children,
+  params: { locale }
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
 
-    ]
-    return (
-        <div>
-            <Navbar defaultLang={locales} title1={t('title1')} signin={t('signin')} signup={t('signup')} rootTitle={t('rootTitle')} search={t("search")} routs={routs} />
+  return (
+    <html lang={locale}>
+      <body className={`${Lastica?.variable} ${Poppins?.variable} ${Satrosi?.variable}`}>
+        <ReduxStoreProvider>
+          <NextIntlClientProvider messages={messages}>
             {children}
-            <Footer />
-        </div>
-    );
-};
-
-export default layout;
+          </NextIntlClientProvider>
+        </ReduxStoreProvider>
+      </body>
+    </html>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,54 +1,25 @@
-import ReduxStoreProvider from '@/provider/ReduxStoreProvider';
-import { Metadata } from 'next';
-import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
-import localFont from "next/font/local";
-import NextAuthSessionProvider from "@/provider/SessionProvider";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import NextAuthSessionProvider from "@/provider/SessionProvider";
+
 
 export const metadata: Metadata = {
   title: "Aristocar",
   description: "Aristocar is car sell gallery",
 };
 
-const Lastica = localFont({
-  src: "../../font/Lastica.ttf",
-  variable: "--font-lastica",
-  weight: "100 400 900",
-});
-const Poppins = localFont({
-  src: "../../font/Poppins-Regular.ttf",
-  variable: "--font-poppins",
-  weight: "100 200 900",
-});
-const Satrosi = localFont({
-  src: "../../font/Satoshi-Regular.ttf",
-  variable: "--font-satoshi",
-  weight: "100 200 300 400 500 600 700 800 900",
-});
 
-export default async function LocaleLayout({
+export default function RootLayout({
   children,
-  params: { locale }
-}: {
+}: Readonly<{
   children: React.ReactNode;
-  params: { locale: string };
-}) {
-  const messages = await getMessages();
-
+}>) {
   return (
-    <html lang={locale}>
-      <body className={`${Lastica?.variable} ${Poppins?.variable} ${Satrosi?.variable}`}>
+    <html lang="en">
+      <body>
         <NextAuthSessionProvider>
-          <ReduxStoreProvider>
-            <NextIntlClientProvider messages={messages}>
-              <div>
-
-                
-                {children}
-              </div>
-            </NextIntlClientProvider>
-          </ReduxStoreProvider>
+          {children}
         </NextAuthSessionProvider>
       </body>
     </html>
