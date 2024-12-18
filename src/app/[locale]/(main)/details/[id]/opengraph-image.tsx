c7dccfad7b28aeff53a2896dbe6b9e2d316cbee7
Hriday-paul <hridoychandrapaul.10@gmail.com>
2024-12-18 11:34:47 +0600

    added normal image in og image

--- src/app/[locale]/(main)/details/[id]/opengraph-image.tsx
+++ src/app/[locale]/(main)/details/[id]/opengraph-image.tsx
@@ -1,5 +1,4 @@
 import { ImageResponse } from 'next/og'
-import ImageN from 'next/image'
 
 export const runtime = 'edge'
 
@@ -29,7 +28,7 @@ export default async function Image() {
           justifyContent: 'center',
         }}
       >
-        <ImageN src='https://aristocar.vercel.app/home/sec1_car.png' alt='car image' />
+        <img src='https://aristocar.vercel.app/home/sec1_car.png' alt='car image' className='h-full w-full' />
         About Acme
       </div>
     ),
