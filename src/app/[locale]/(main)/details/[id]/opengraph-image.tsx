79442ca12ca707667e48bb9791339e671f5ee325
Hriday-paul <hridoychandrapaul.10@gmail.com>
2024-12-18 11:18:13 +0600

    added og image extension

--- src/app/[locale]/(main)/details/[id]/opengraph-image.tsx
+++ src/app/[locale]/(main)/details/[id]/opengraph-image.tsx
@@ -20,7 +20,7 @@ export default async function Image() {
       <div
         style={{
           fontSize: 128,
-          backgroundImage: './home/sec1_car',
+          backgroundImage: './home/sec1_car.png',
           width: '100%',
           height: '100%',
           display: 'flex',
