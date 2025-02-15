1522793a3e95ec0fa7a32a705f65aaf286aa252f
Hriday-paul <hridoychandrapaul.10@gmail.com>
2025-02-15 12:44:56 +0600

    added root og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+//og-logo
+
 export const metadata: Metadata = {
   title: "Aristocar",
   description: "Browse the best luxury cars, exotic cars, supercars, hypercars, and rare cars for sale from top dealers around the world",
+  metadataBase: new URL(process.env.MY_DOMAIN!),
+  openGraph: {
+    title: "Aristocar",
+    description: "Browse the best luxury cars, exotic cars, supercars, hypercars, and rare cars for sale from top dealers around the world",
+    images: ['/og-logo.png'],
+  },
 };
 
 
--- src/utils/LoginAction.ts
+++ src/utils/LoginAction.ts
@@ -4,7 +4,7 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 export const GoogleLogin = async (): Promise<{ displayName: string | null, email: string | null, photoURL: string | null }> => {
     const provider = new GoogleAuthProvider();
 
-    const { user } = await signInWithPopup(auth, provider);
-    const { displayName, email, photoURL } = user
+    const res = await signInWithPopup(auth, provider);
+    const { displayName, email, photoURL } = res.user
     return { displayName, email, photoURL }
 };
